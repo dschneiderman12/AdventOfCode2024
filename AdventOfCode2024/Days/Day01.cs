8db75b82acd361684e9a4d67d5481a0822988668
Daniel Schneiderman <DSchneiderman@leafhome.com>
2024-12-17 08:18:16 -0500

    inital push with Day 01

---
+++ AdventOfCode2024/Days/Day01.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2024.Days
+{
+    internal class Day01
+    {
+        public List<int> left { get; set; } = new List<int>();
+        public List<int> right { get; set; } = new List<int>();
+
+        public void Setup()
+        {
+            string filePath = @"Inputs\Day01.txt";
+            var input = Path.Combine(AppContext.BaseDirectory, filePath);
+
+            StreamReader sr = new StreamReader(input);
+            string line = sr.ReadLine();
+
+            while (line != null)
+            {
+                string[] split = line.Split(new char[0], StringSplitOptions.RemoveEmptyEntries);
+
+                left.Add(int.Parse(split[0]));
+                right.Add(int.Parse(split[1]));
+
+                line = sr.ReadLine();
+            }
+
+            sr.Close();
+        }
+
+        public int PartOne(List<int> left, List<int> right)
+        {
+            int result = 0;
+
+            left.Sort();
+            right.Sort();
+
+            for (int i = 0; i < left.Count; i++)
+            {
+                int diff = left[i] - right[i];
+
+                result += Math.Abs(diff);
+            }
+
+            return result;
+        }
+
+        public int PartTwo(List<int> left, List<int> right)
+        {
+            int result = 0;
+
+            foreach (int i in left)
+            {
+                int count = 0;
+
+                for (int j = 0; j < right.Count; j++)
+                {
+                    if (i == right[j])
+                        count++;
+                }
+
+                result += i * count;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ AdventOfCode2024/Program.cs
@@ -0,0 +1,34 @@
+﻿using AdventOfCode2024.Days;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("------------------------------");
+            Console.WriteLine("--------Advent of Code--------");
+            Console.WriteLine("------------------------------");
+            Console.WriteLine();
+
+            #region Day One
+            Day01 day01 = new Day01();
+            day01.Setup();
+            string answerOne = day01.PartOne(day01.left, day01.right).ToString();
+            string answerTwo = day01.PartTwo(day01.left, day01.right).ToString();
+
+            Console.WriteLine("Day 01 - part one: " + answerOne);
+            Console.WriteLine("Day 01 - part two: " + answerTwo);
+            #endregion
+
+            Console.WriteLine();
+            Console.WriteLine("------------------------------");
+        }
+    }
+}
